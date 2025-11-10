c99a770f066d3a8eda436fb9503647c33a59af6e
akWork <ak.work1233@gmail.com>
2025-11-10 16:02:55 +0500

    Исправит команду в планировщике задач

--- src/app/Console/Handler.php
+++ src/app/Console/Handler.php
@@ -9,9 +9,9 @@ class Handler
     public function __invoke(Schedule $schedule): void
     {
         $scheduleTimes = explode(',', env('WB_SCHEDULE_TIME', '9,17'));
-        $schedule->command('wb:fetch-all --limit=500'
-            . ' --dateFrom=' .env('WB_DATE_FROM', '2025-11-01')
-            . ' --dateTo=' .env('WB_DATE_TO', '2025-11-03')
+        $schedule->command('wb:fetch-all 1 --limit=500'
+            . ' --dateFrom=' .env('WB_DATE_FROM', '2025-11-09')
+            . ' --dateTo=' .env('WB_DATE_TO', '2025-11-10')
             . ' -v'
             )
             ->twiceDaily($scheduleTimes[0], $scheduleTimes[1])
